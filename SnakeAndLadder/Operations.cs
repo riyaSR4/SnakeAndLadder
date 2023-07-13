97bc2c18a940fa66bdfa0b0f9e33519c1b4699dd
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-13 11:45:11 +0530

    [Riya] Add : Implemented Start Position

---
+++ SnakeAndLadder/Operations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Operations
+    {
+        int position = 0;
+    }
+}
